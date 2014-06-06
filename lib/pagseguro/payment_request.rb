0384aafb4608641cfb770097337db0f6f50e4840
Caio Fellipe <caiofct@gmail.com>
2014-06-05 21:32:38 -0300

    Fixed syntax issues with new funcionalities for subscriptions pre-approval

--- lib/pagseguro/payment_request.rb
+++ lib/pagseguro/payment_request.rb
@@ -101,40 +101,13 @@ module PagSeguro
 
     # Calls the PagSeguro web service and register this request for payment.
     def register
-      if 
       params = Serializer.new(self).to_params.merge({
         email: email,
         token: token
       })
       Response.new Request.post("checkout", params)
-      
-      attr_accessor :pre_approval_charge
-    
-    attr_accessor :pre_approval_name
-    
-    attr_accessor :pre_approval_details
-    
-    attr_accessor :pre_approval_amount_per_payment
-    
-    attr_accessor :pre_approval_period
-    
-    attr_accessor :pre_approval_initial_date
-    
-    attr_accessor :pre_approval_final_date
-    
-    attr_accessor :pre_approval_max_amount_per_period
-    
-    attr_accessor :pre_approval_max_total_amount
-    
-    attr_accessor :review_url
-    
-    attr_accessor :pre_approval_day_of_year
-    
-    attr_accessor :pre_approval_day_of_month
-    
-    attr_accessor :pre_approval_day_of_week
     end
-
+      
     private
     def before_initialize
       self.currency = "BRL"
